047123b2010ef78de5ef0b488642145998ef5795
Ville Taskinen <v.taski@gmail.com>
2019-11-21 23:25:32 +0200

    Lajin lisäys ja yhteenvetokysely

--- application/views.py
+++ application/views.py
@@ -6,4 +6,5 @@ from application.auth.models import User
 
 @app.route('/')
 def index():
-    return render_template("index.html", max_plants=User.find_users_with_max_plants())    
+    return render_template("index.html")    
+#        return render_template("index.html", max_plants=User.find_users_with_max_plants())    
