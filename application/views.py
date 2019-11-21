e877f4ff7aadad1966b71c5f1d6d9f8fb508a82e
Ville Taskinen <v.taski@gmail.com>
2019-11-21 23:35:26 +0200

    Yhteenvetokyselyn päivitys

--- application/views.py
+++ application/views.py
@@ -6,5 +6,5 @@ from application.auth.models import User
 
 @app.route('/')
 def index():
-    return render_template("index.html")    
+    return render_template("index.html", no_plants=User.find_users_with_no_plants())    
 #        return render_template("index.html", max_plants=User.find_users_with_max_plants())    
